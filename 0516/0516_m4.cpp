7fc3106abb65391aac0ca0bcb6ed92b195ee9d5f
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-05-17 15:10:20 +0900

    dd

--- 0516/0516_m4.cpp
+++ 0516/0516_m4.cpp
@@ -11,6 +11,7 @@ public:
 	string m_name;
 	string m_company;
 	Snack() {};
+
 	Snack(int price, int name, string company)
 	{
 		m_price = price;
@@ -18,7 +19,8 @@ public:
 		m_company = company;
 	}
 
-	string getName() { return m_name; }
+	virtual void name() = 0;
+
 };
 
 class Candy : public Snack
@@ -33,6 +35,7 @@ public:
 		m_company = company;
 		m_taste = taste;
 	}
+	void name() override { cout << "상품이름 : " << m_name << endl; }
 };
 
 class Chocolate : public Snack
@@ -47,20 +50,35 @@ public:
 		m_company = company;
 		m_shape = shape;
 	}
+	void name() override { cout << "상품이름 :" << m_name << endl; }
 };
 
-void main()
+int main()
 {
-	vector<string> snackBasket;
+	Snack* candy1 = new Candy(1000, "딸기사탕", "츄파춥스", "Sweet");
+	Snack* candy2 = new Candy(500, "초코사탕", "롤리팝", "롤리");
+	Snack* chocolate1 = new Chocolate(2000, "가나초코", "가나", "DarkChoco");
+	Snack* chocolate2 = new Chocolate(1500, "페레로로쉐", "페레로", "초코");
+	
+	Snack* snackbasket[4] = { candy1, candy2, chocolate1, chocolate2 };
 
-	Candy candy(1000, "딸기사탕", "츄파춥스", "Sweet");
-	Chocolate chocolate(2000, "가나초코", "가나", "DarkChoco");
-
-	snackBasket.push_back(candy.getName());
-	snackBasket.push_back(chocolate.getName());
-
-	for (auto& name : snackBasket)
+	for (int i = 0; i < sizeof(snackbasket) / sizeof(snackbasket[0]); i++)
 	{
-		cout << name << endl;
+		snackbasket[i]->name();
 	}
+	
+
+	return 0;
 }
+
+/*
+heap 
+-> 프로그램이 실행되는 중에 사용되는 메모리
+-> 100% 사용자가 고나리 (new, delete)
+-> 조금 더 느림
+
+stack
+->프로그램 실행 전 할당 메모리
+->new 키워드 외 모든것
+->조금 더 빠름, 운영체제 별로 메모리 용량 한계
+*/
